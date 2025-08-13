58b79efd2f3148d1111f5f370d61979a44be4666
220-12\hyejin <h77200407@gmail.com>
2025-08-13 17:45:47 +0900

    디버깅 완

--- src/main/java/org/mbc/czo/function/Sample/repository/SampleJpaRepository.java
+++ src/main/java/org/mbc/czo/function/Sample/repository/SampleJpaRepository.java
@@ -3,13 +3,5 @@ package org.mbc.czo.function.Sample.repository;
 import org.mbc.czo.function.Sample.domain.Sample;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-public interface SampleJpaRepository extends JpaRepository<Sample, Long>{
-=======
 public interface SampleJpaRepository extends JpaRepository<Sample, String>{
->>>>>>> origin/LHW
-=======
-public interface SampleJpaRepository extends JpaRepository<Sample, Long>{
->>>>>>> 1bd0154d4c474243ecef611a9f954a9702f10bf6
 }
