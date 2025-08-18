90801a212d5456eab9d9f936dd98e55f23b1445c
hyejin-jun <h77200407@gmail.com>
2025-08-18 09:38:50 +0900

    Delete src directory

--- src/main/java/org/mbc/czo/function/Sample/repository/SampleJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package org.mbc.czo.function.Sample.repository;
-
-import org.mbc.czo.function.Sample.domain.Sample;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SampleJpaRepository extends JpaRepository<Sample, String>{
-}
--- src/main/java/org/mbc/czo/function/product/exception/OutOfStockException.java
+++
@@ -1,9 +0,0 @@
-package org.mbc.czo.function.product.exception;
-
-public class OutOfStockException extends RuntimeException{
-
-    public OutOfStockException(String message) {
-        super(message);
-    }
-
-}
